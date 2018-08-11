8fa02c01ad415e72ab9588d1b7b97039f09c05b9
zuku <zekumisufe@zep-hyr.com>
2018-08-11 16:53:45 +0430

    added config values and defaults in __main__

--- telegram_export/__main__.py
+++ telegram_export/__main__.py
@@ -59,6 +59,7 @@ def load_config(filename):
         'SessionName': 'exporter',
         'OutputDirectory': '.',
         'MediaWhitelist': 'chatphoto, photo, sticker',
+        'DialogWhitelist': 'user, group',
         'MaxSize': '1MB',
         'LogLevel': 'INFO',
         'DBFileName': 'export',
